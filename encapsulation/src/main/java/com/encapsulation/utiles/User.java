c5b16c2e9c0008a45ec8a837ecc975cc71c8e8bb
nando-z <157473087+nando-z@users.noreply.github.com>
2025-07-09 11:39:50 -0700

    write encapsulation

---
+++ encapsulation/src/main/java/com/encapsulation/utiles/User.java
@@ -0,0 +1,33 @@
+package com.encapsulation.utiles;
+
+public class User {
+    private String username;
+    private String password;
+    private String uuid; 
+    
+    // Setters
+    public void setUser(String username) {
+        this.username = username;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+    
+    // Getters
+    public String getUser() {
+        return username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public String getUuid() {
+        return uuid;
+    }
+}
